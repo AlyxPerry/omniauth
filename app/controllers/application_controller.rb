21de2835fb4677748447450514ada09676e112e8
Alyx Perry <alyxperry1@gmail.com>
2019-04-17 18:48:27 -0400

    add

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+  	if(session[:user_id])
+  		if(!@current_user)
+  			@current_user = User.find(session[:user_id ])
+  		end
+  		return @current_user
+  	end
+  end
+
+end
